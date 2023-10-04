bc0feab45db9e0f5c315399ccf0a77e83e750683
zSkillCode <68539499+zSkillCode@users.noreply.github.com>
2023-10-04 20:46:55 +0200

    Update dist

--- 08-world-wise/src/components/Logo.jsx
+++ 08-world-wise/src/components/Logo.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Logo = () => {
 	return (
 		<Link to="/">
-			<img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
+			<img src="logo.png" alt="WorldWise logo" className={styles.logo} />
 		</Link>
 	);
 };
